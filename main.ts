42eace0062da467138ce9b4f3d45a3e0eac4786f
Austin Nhung <austinnhung@yahoo.ca>
2023-10-15 17:11:08 -0600

    Autoformat to specified character limit

--- main.ts
+++ main.ts
@@ -1,85 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface BrakelineFormatterSettings {
+	characterLimit: int;
+	// formatOnEdit: bool
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+
+const DEFAULT_SETTINGS: BrakelineFormatterSettings = {
+	characterLimit: 80,
+	// formatOnEdit: false,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+export default class BrakelineFormatter extends Plugin {
+	settings: BrakelineFormatterSettings;
 
 	async onload() {
+		// Runs whenever the user starts using the plugin
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: "format",
+			name: "Run autoformatter",
+			editorCallback: (editor: Editor) => {
+				// Only run if in markdown
+				const is_markdown = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (!is_markdown) {
+					return;
 				}
+
+				// Retrieve lines from editor
+				const text: string = editor.getValue();
+				const formatted: string = this.formatText(text);
+
+				// Replace text in editor
+				editor.setValue(formatted);
 			}
-		});
+		})
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// This adds a settings tab so the user can configure various aspects
+		// of the plugin
+		this.addSettingTab(new BrakelineFormatterSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// When registering intervals, this function will automatically clear
+		// the interval when the plugin is disabled.
+		this.registerInterval(
+			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
+		);
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,28 +61,72 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	formatText(text: string): string {
+		// Format text
+		const paragraphs: string[] = text.split('\n');
+		let result: string[] = [];
+
+		for (let paragraph of paragraphs) {
+			// Preserve existing indent
+			let current: string = this.getLeadingSpaces(paragraph);
+			let indent: string = current;
+			const trimmed: string = paragraph.trimStart();
+
+			// Add indent for notes
+			if (trimmed.startsWith('- ')) {
+				indent += ' '.repeat(2);
+			}
+
+			// Add indent for numbered lists
+			let numbered_list = trimmed.match(/^(\d+)\.\s/)
+
+			if (numbered_list) {
+				indent += ' '.repeat(numbered_list[0].length);
+			}
+
+			// Format paragraph
+			const words: string[] = trimmed.split(' ');
+
+			for (const word of words) {
+				const length: number = current.length + word.length + 1;
+				if (length <= this.settings.characterLimit) {
+					if (current.length > 0 && !current.match(/\s$/)) {
+						current += ' ';
+					}
+
+					current += word;
+				}
+				else {
+					result.push(current);
+					current = indent + word;
+				}
+			}
+
+			result.push(current);
+		}
+
+		const formatted: string = result.join('\n');
+
+		return formatted;
+	}
+
+	getLeadingSpaces(text: string): string {
+		const match = text.match(/^\s*/);
+
+		if (match) {
+			return match[0];
+		}
+
+		return '';
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class BrakelineFormatterSettingTab extends PluginSettingTab {
+	plugin: BrakelineFormatter;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: BrakelineFormatter) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +137,16 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+			.setName("Character Limit")
+			.setDesc("The maximum number of characters per line.")
+			.addSlider(val => val
+				.setLimits(60, 150, 1)
+				.setValue(this.plugin.settings.characterLimit)
+				.setDynamicTooltip()
+				.onChange(async (value: number) => {
+					this.plugin.settings.characterLimit = value;
 					await this.plugin.saveSettings();
-				}));
+				})
+			);
 	}
 }
