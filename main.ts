63be8f3c91d25aa344c69db1651439751021cbb4
Austin Nhung <austinnhung@yahoo.ca>
2023-10-16 00:51:13 -0600

    Adding link parsing

--- main.ts
+++ main.ts
@@ -1,14 +1,62 @@
 import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 
+interface MatchGroup {
+	// The raw string value
+	text: string;
+	// The nominal length of the formatted string value, not the length of the
+	// raw string value
+	length?: number;
+}
+
+
+class StringGroup implements MatchGroup {
+	constructor(text: string) {
+		this.text = text;
+		this.length = text.length;
+	}
+}
+
+
+class MarkdownLinkGroup implements MatchGroup {
+	constructor(text: string) {
+		this.text = text;
+
+		// TODO: check this regex
+		const match = text.match(/\[(.*?)\]/);
+
+		if (match) {
+			this.length = match.length;
+		}
+	}
+}
+
+class WikilinkGroup implements MatchGroup {
+	constructor(text: string) {
+		this.text = text;
+
+		// TODO: check this regex
+		const match = text.match(/\[.*?|(.*?)\]/);
+
+		if (match) {
+			this.length = match.length;
+		}
+	}
+}
+
+
 interface BrakelineFormatterSettings {
 	characterLimit: int;
+	ignoreWikiLinks: bool;
+	ignoreMarkdownLinks: bool;
 	// formatOnEdit: bool
 }
 
 
 const DEFAULT_SETTINGS: BrakelineFormatterSettings = {
 	characterLimit: 80,
+	ignoreWikiLinks: true,
+	ignoreMarkdownLinks: true,
 	// formatOnEdit: false,
 }
 
@@ -31,11 +79,9 @@ export default class BrakelineFormatter extends Plugin {
 					return;
 				}
 
-				// Retrieve lines from editor
+				// Retrieve, format, then replace text in editor
 				const text: string = editor.getValue();
 				const formatted: string = this.formatText(text);
-
-				// Replace text in editor
 				editor.setValue(formatted);
 			}
 		})
@@ -70,24 +116,16 @@ export default class BrakelineFormatter extends Plugin {
 		for (let paragraph of paragraphs) {
 			// Preserve existing indent
 			let current: string = this.getLeadingSpaces(paragraph);
-			let indent: string = current;
 			const trimmed: string = paragraph.trimStart();
+			let indent: string = current + this.getIndent(trimmed);
 
-			// Add indent for notes
-			if (trimmed.startsWith('- ')) {
-				indent += ' '.repeat(2);
-			}
-
-			// Add indent for numbered lists
-			let numbered_list = trimmed.match(/^(\d+)\.\s/)
-
-			if (numbered_list) {
-				indent += ' '.repeat(numbered_list[0].length);
-			}
-
-			// Format paragraph
+			// Split words over link formats
+			// let words: MatchGroup[] = [StringGroup(trimmed)];
+			// words = this.splitGroups(words, this.splitMarkDownLink);
+			// words = this.splitGroups(words, this.splitWikilink);
 			const words: string[] = trimmed.split(' ');
 
+			// Format paragraph
 			for (const word of words) {
 				const length: number = current.length + word.length + 1;
 				if (length <= this.settings.characterLimit) {
@@ -111,6 +149,24 @@ export default class BrakelineFormatter extends Plugin {
 		return formatted;
 	}
 
+	getIndent(text: string): string {
+		let indent: number = 0;
+
+		// Add indent for notes
+		if (trimmed.startsWith('- ')) {
+			indent += 2;
+		}
+
+		// Add indent for numbered lists
+		let numbered_list = text.match(/^(\d+)\.\s/)
+
+		if (numbered_list) {
+			indent += numbered_list[0].length;
+		}
+
+		return ' '.repeat(indent);
+	}
+
 	getLeadingSpaces(text: string): string {
 		const match = text.match(/^\s*/);
 
@@ -119,6 +175,76 @@ export default class BrakelineFormatter extends Plugin {
 		}
 
 		return '';
+	}
+
+	splitGroups(
+		groups: MatchGroup[],
+		splitCallback: (text: string) => MatchGroup[]
+	): MatchGroup[] {
+		let result: MatchGroup[] = [];
+
+		for (group of groups) {
+			if (group instanceof StringGroup) {
+				result += splitCallback(group.text);
+			}
+			else {
+				result.push(group);
+			}
+		}
+
+		return result;
+	}
+
+	splitMarkdownLink(text: string): MatchGroup[] {
+		const re_markdown: RegExp = /\[.*?\]\(.*?\)/g;
+
+		const not_links: string[] = text.split(re_markdown);
+		let links = text.match(re_markdown);
+
+		if (!links) {
+			links = [];
+		}
+
+		const i_max: number = Math.max(not_links.length, links.length);
+		let result: MatchGroup[] = [];
+
+		for (let i = 0; i < i_max; i++) {
+			if (i < not_links.length) {
+				result.push(StringGroup(not_links[i]));
+			}
+
+			if (i < links.length) {
+				result.push(MarkdownLinkGroup(links[i]));
+			}
+		}
+
+		return result;
+	}
+
+	splitWikilink(text: string): MatchGroup[] {
+		const re_wikilink: RegExp = /\[\[.*?\]\]/g;
+
+		const not_links: stirng[] = text.split(re_wikilink);
+		let links = text.match(re_wikilink);
+
+		if (!links) {
+			links = [];
+		}
+
+		const i_max: number = Math.max(not_links.length, links.length);
+		let result: MatchGroup[] = [];
+
+		for (let i = 0; i < i_max; i++) {
+			if (i < not_links.lenght) {
+				result.push(StringGroup(not_links[i]));
+			}
+
+			if (i < links.length) {
+				result.push(Wikilink(links[i]));
+			}
+		}
+
+		return result;
 	}
 }
 
@@ -138,13 +264,35 @@ class BrakelineFormatterSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName("Character Limit")
-			.setDesc("The maximum number of characters per line.")
+			.setDesc("The maximum number of characters per line")
 			.addSlider(val => val
 				.setLimits(60, 150, 1)
 				.setValue(this.plugin.settings.characterLimit)
 				.setDynamicTooltip()
 				.onChange(async (value: number) => {
 					this.plugin.settings.characterLimit = value;
+					await this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Ignore wikilinks")
+			.setDesc("If enabled, wikilink style links will not count toward the character limit")
+			.addToggle(val => val
+				.setValue(this.plugin.settings.ignoreWikiLinks)
+				.onChange(async (value: bool) => {
+					this.plugin.settings.ignoreWikiLinks = value;
+					await this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Ignore markdown links")
+			.setDesc("If enabled, markdown style links will not count toward the character limit")
+			.addToggle(val => val
+				.setValue(this.plugin.settings.ignoreMarkdownLinks)
+				.onChange(async (value: bool) => {
+					this.plugin.settings.ignoreMarkdownLinks = value;
 					await this.plugin.saveSettings();
 				})
 			);
